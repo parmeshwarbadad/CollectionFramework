4fbb3a1ceced79bcda4e39ab8eb8ff8c3925c0f0
Parmeshwar Badad <107673796+parmeshwarbadad@users.noreply.github.com>
2022-07-23 13:21:56 +0530

    DemoDictionary.cs

---
+++ DemoDictionary.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CollectionFramework
+{
+    class DemoDictionary
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> dd = new Dictionary<string, int>();
+            dd.Add("Param", 89);
+            dd.Add("Anuj", 87);
+            dd.Add("Ajinkya", 99);
+            dd.Add("Pralhad", 87);
+            dd.Add("Om", 99);
+
+            dd["Ajinkya"] = 90;
+            dd.Remove("Anuj");
+
+            Console.WriteLine(dd.Count);
+            //dd.clear();
+            Console.WriteLine(dd.ContainsKey ("Om"));
+            Console.WriteLine(dd.ContainsValue (87));
+
+            //List<string> keys = dd.Keys.ToList();
+
+            Console.WriteLine(dd["Pralhad"]);
+
+            foreach (KeyValuePair <string ,int> kv in dd)
+                Console.WriteLine(kv.Key+" "+kv.Value);
+
+        }
+    }
+}
