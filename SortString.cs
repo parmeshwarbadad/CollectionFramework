0a84fbdfa0f0590c002f6c773a394d6b425b1305
Parmeshwar Badad <107673796+parmeshwarbadad@users.noreply.github.com>
2022-07-23 13:30:13 +0530

    SortString.cs

---
+++ SortString.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CollectionFramework
+{
+    class SortString
+    {
+        static bool Isadd(int n)
+        {
+            return n % 2 == 1;
+        }
+        static void Main(string[] args)
+        {
+           /* List<string> lst = new List<string>() { "deepa", "Medha", "Kirti" };
+            lst.Sort();
+            lst.Reverse();
+            foreach(string s in lst)
+                Console.WriteLine(s+" ");*/
+
+            List<int> l2 = new List<int>() { 23, 44, 56, 77, 89, 90, 53 };
+            List<int> oddlist = l2.FindAll(Isadd);
+
+            foreach (int s in oddlist)
+                Console.WriteLine(s + " ");
+
+
+        }
+    }
+}
